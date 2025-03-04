9fd7c7edbf9cef2a39ec56d283beeba5e8394652
thloc2905 <buil07184@gmail.com>
2025-03-04 19:52:06 +0700

    add file demo

---
+++ CountOddNumbersInIntervalRange.cpp
@@ -0,0 +1,13 @@
+
+class Solution {
+    public:
+        int countOdds(int low, int high) {
+            int count = 0;
+            for(int i = low ; i <= high ;i++){//duyệt qua từng phần tử từ low -> highhigh
+                if( i % 2 != 0){//phần từ này duyệt số lẽ 
+                    count++;
+                }
+            }
+            return count;
+        }
+    };
