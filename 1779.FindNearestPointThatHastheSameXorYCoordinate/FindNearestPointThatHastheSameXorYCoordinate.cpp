6a8a44e6b96b782a78cfd83317ec394441bbf6c9
thloc2905 <buil07184@gmail.com>
2025-03-25 21:18:17 +0700

    Create FindNearestPointThatHastheSameXorYCoordinate.cpp

---
+++ 1779.FindNearestPointThatHastheSameXorYCoordinate/FindNearestPointThatHastheSameXorYCoordinate.cpp
@@ -0,0 +1,22 @@
+    class Solution {
+    public:
+        int nearestValidPoint(int x, int y, vector<vector<int>>& points) {
+            int max = INT_MAX,index = -1;
+            for(int i = 0; i < points.size();i++){
+                int xi = points[i][0];
+                int yi = points[i][1];
+            int dist;
+            if((x == xi)||(y == yi)){
+                int dist = abs(x -xi)+abs(y-yi);
+            
+            if(max > dist ){
+                max = dist ;
+                index = i;
+            }
+    
+        }
+        }
+        return index;
+        }
+        
+    };
