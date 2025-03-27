9663bc01205b15879150ffdf870c63aa46fd182f
thloc2905 <buil07184@gmail.com>
2025-03-27 22:36:18 +0700

    Create FindtheIndexoftheFirstOccurrenceinaString.cpp

---
+++ 28.FindtheIndexoftheFirstOccurrenceinaString/FindtheIndexoftheFirstOccurrenceinaString.cpp
@@ -0,0 +1,14 @@
+//28. Find the Index of the First Occurrence in a String
+class Solution {
+    public:
+        int strStr(string haystack, string needle) {
+            for(int i =0; i < haystack.size();i++){
+                if(haystack[i] == needle[0]){
+                   if(haystack.substr(i,needle.size()) == needle){
+                        return i;
+                    }
+                }
+            }
+            return -1;
+        }
+    };
