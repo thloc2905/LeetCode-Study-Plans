20fc926e92378122e72fb5e4caefd70342da231f
thloc2905 <buil07184@gmail.com>
2025-04-04 19:32:50 +0700

    Create Sqrt(x).cpp

---
+++ 69.Sqrt(x)/Sqrt(x).cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(sqrt(x)<0)
+        {
+            return false;
+        }
+        int count = sqrt(x);
+        return count;
+    }
+};
