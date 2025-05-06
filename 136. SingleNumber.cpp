6c9b07c42a5a7fa9cc60ac851cdad42458f57939
thloc2905 <buil07184@gmail.com>
2025-05-06 08:28:18 +0700

    Create 136. SingleNumber.cpp

---
+++ 136. SingleNumber.cpp
@@ -0,0 +1,11 @@
+//bài 136. Single Number
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result=0;
+        for(int num :nums)
+        {
+            result^=num;
+        }
+        return result;
+    }
+}
